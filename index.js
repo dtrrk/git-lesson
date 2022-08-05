68f2ab97c906787087d73e578a47f907f70346ae
GennadyAC <gennady.v@advancedcommunities.com>
2022-08-05 11:46:44 +0300

    Julia translator

--- index.js
+++ index.js
@@ -1,9 +1,100 @@
-c1
-c2
-c3
-c4
-c5
-c6
-c7
-c8
-c9
+//
+// function googleTranslateElementInit() {
+//     new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'en,ru,be,it', autoDisplay: false}, 'google_translate_element');
+// }
+//
+// function TranslateClearCookie(lang) {
+//     document.cookie = `googtrans=/en/${lang};path=/`;
+//     // document.cookie = `googtrans=/en/it;path=/git-lesson`;
+// }
+//
+// document.addEventListener("DOMContentLoaded", () => {
+//
+//     let lang = 'en';
+//     if (document.cookie) {
+//         const cookies = document.cookie.split(';')
+//         console.log(cookies)
+//         for (const cookie of cookies) {
+//             let arr = cookie.split('=')
+//             if (arr[0] === 'googtrans') {
+//                 lang = arr[1].slice(4)
+//             }
+//         }
+//     } else {
+//         return false
+//     }
+//
+//     const googleOptions = document.getElementById('custom-select').querySelectorAll('option')
+//     for (let i = 0; i < googleOptions.length; i++) {
+//         if (googleOptions[i].value === lang) {
+//             googleOptions[i].selected = true;
+//         }
+//     }
+// });
+//
+//
+// function translateLanguage(lang) {
+//     googleTranslateElementInit()
+//     TranslateClearCookie(lang)
+//     document.location.reload()
+// }
+
+
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'en,ru,be,it', autoDisplay: false}, 'google_translate_element');
+}
+
+function translateLanguage(lang) {
+    googleTranslateElementInit()
+
+    const $frame = document.querySelector('.goog-te-menu-frame');
+
+    const $googleOptions = document.getElementById('google_translate_element').querySelectorAll('select option')
+    let fullName = ''
+    Array.from($googleOptions).find(option => {
+        if (option.value == lang) {
+            option.click()
+            option.selected = true;
+            fullName = option.textContent
+        }
+    })
+
+    if (!$frame) {
+        document.cookie = `googtrans=/en/${lang};path=/`;
+        document.location.reload()
+    }
+
+    const googleOptions = $frame.contentWindow.document.body.querySelectorAll('.goog-te-menu2 span.text')
+    for (const option of googleOptions) {
+        if (option.textContent === fullName) {
+            option.click()
+            option.selected = true;
+        }
+    }
+
+    if (lang == 'en') {
+        document.location.reload()
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    let lang = 'en';
+    if (document.cookie) {
+        const cookies = document.cookie.split(';')
+        for (const cookie of cookies) {
+            let arr = cookie.split('=')
+            if (arr[0] === 'googtrans') {
+                lang = arr[1].slice(4)
+            }
+        }
+    } else {
+        return false
+    }
+
+    const googleOptions = document.getElementById('custom-select').querySelectorAll('option')
+    for (let i = 0; i < googleOptions.length; i++) {
+        if (googleOptions[i].value === lang) {
+            googleOptions[i].selected = true;
+        }
+    }
+});
